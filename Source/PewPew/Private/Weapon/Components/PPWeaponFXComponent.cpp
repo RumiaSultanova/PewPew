7396a8bea30e7e92c9f14968b0433cf19a001604
Rumia <rumia.slt@gmail.com>
2024-01-11 10:49:37 +0300

    on weapon hit decals added

--- Source/PewPew/Private/Weapon/Components/PPWeaponFXComponent.cpp
+++ Source/PewPew/Private/Weapon/Components/PPWeaponFXComponent.cpp
@@ -4,6 +4,8 @@
 #include "Weapon/Components/PPWeaponFXComponent.h"
 #include "NiagaraFunctionLibrary.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/DecalComponent.h"
 
 UPPWeaponFXComponent::UPPWeaponFXComponent()
 {
@@ -12,17 +14,33 @@ UPPWeaponFXComponent::UPPWeaponFXComponent()
 
 void UPPWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
 {
-	auto Effect = DefaultEffect;
+	auto ImpactData = DefaultImpactData;
 
 	if (Hit.PhysMaterial.IsValid())
 	{
 		const auto PhysMat = Hit.PhysMaterial.Get();
-		if (EffectsMap.Contains(PhysMat))
+		if (ImpactDataMap.Contains(PhysMat))
 		{
-			Effect = EffectsMap[PhysMat];
+			ImpactData = ImpactDataMap[PhysMat];
 		}
 	}
 
-	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
- }
+	// niagara
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(),
+		ImpactData.NiagaraEffect,
+		Hit.ImpactPoint,
+		Hit.ImpactNormal.Rotation());
+
+	// decal
+	auto DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(),
+		ImpactData.DecalData.Material,
+		ImpactData.DecalData.Size,
+		Hit.ImpactPoint,
+		Hit.ImpactNormal.Rotation());
+
+	if (DecalComponent)
+	{
+		DecalComponent->SetFadeOut(ImpactData.DecalData.LifeTime, ImpactData.DecalData.FadeOutTime);
+	}
+}
 
--- Source/PewPew/Public/PPCoreTypes.h
+++ Source/PewPew/Public/PPCoreTypes.h
@@ -50,3 +50,37 @@ struct FWeaponUIData
 
 DECLARE_MULTICAST_DELEGATE(FOnDeathSignature);
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float);
+
+//VFX
+
+class UNiagaraSystem;
+
+USTRUCT(Blueprintable)
+struct FDecalData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	UMaterialInterface* Material;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	FVector Size = FVector(10.0f);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	float LifeTime = 5.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	float FadeOutTime = 0.7f;
+};
+
+USTRUCT(Blueprintable)
+struct FImpactData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	UNiagaraSystem* NiagaraEffect;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+	FDecalData DecalData;
+};
