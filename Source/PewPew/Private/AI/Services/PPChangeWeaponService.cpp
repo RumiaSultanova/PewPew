5f7648ea93c5fafe188806f8dd4fe4719713751d
Rumia <rumia.slt@gmail.com>
2024-01-29 12:43:03 +0300

    ai change weapon service added

---
+++ Source/PewPew/Private/AI/Services/PPChangeWeaponService.cpp
@@ -0,0 +1,28 @@
+// Pew-Pew Game. All Rights Reserved.
+
+#include "AI/Services/PPChangeWeaponService.h"
+#include "Components/PPWeaponComponent.h"
+#include "AIController.h"
+#include "PPUtils.h"
+
+
+UPPChangeWeaponService::UPPChangeWeaponService()
+{
+	NodeName = "Change Weapon";
+}
+
+void UPPChangeWeaponService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	const auto Controller = OwnerComp.GetAIOwner();
+	if (Controller)
+	{
+		const auto WeaponComponent = PPUtils::GetPPPlayerComponent<UPPWeaponComponent>(Controller->GetPawn());
+		if (WeaponComponent && Probability > 0 && FMath::FRand() <= Probability)
+		{
+			WeaponComponent->NextWeapon();
+		}
+	}
+
+
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
---
+++ Source/PewPew/Public/AI/Services/PPChangeWeaponService.h
@@ -0,0 +1,22 @@
+// Pew-Pew Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "PPChangeWeaponService.generated.h"
+
+UCLASS()
+class PEWPEW_API UPPChangeWeaponService : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	UPPChangeWeaponService();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta=(ClampMin = "0.0", ClampMax = "1.0"))
+	float Probability = 0.5f;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
